180b3474c57f6a061ef2886a13248f3db406b9e2
vilaschandran1 <vilaschandran1@gmail.com>
2026-05-23 16:46:24 +0530

    Optimize package loading in R script
    Replaced package installation with conditional loading to improve efficiency.

--- 01_Main_Inidan_Macro_Fiscal_Analysis.R
+++ 01_Main_Inidan_Macro_Fiscal_Analysis.R
@@ -1,12 +1,9 @@
 RStudio Version-2025.09.2+418 "Cucumberleaf Sunflower" Release (12f6d5e22720bd78dbd926bb344efe12d0dce83d, 2025-10-20) for windows
 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) RStudio/2025.09.2+418 Chrome/138.0.7204.251 Electron/37.6.1 Safari/537.36, Quarto 1.7.32
 set.seed(139)
-install.packages("urca")   #package for zivot-andrews unit root test
-install.packages("meboot") #package for maximum entropy bootstrap 
-install.packages("philentropy") #package for Jensen-Shannon Divergence Calculation
-library(meboot) 
-library(urca)
-library(philentropy)
+if(!require("urca")) install.packages("urca")
+if(!require("meboot")) install.packages("meboot")
+if(!require("philentropy")) install.packages("philentropy")
 GDP_REAL=c(8736329,9213017,9801370,10527674,11369493,12308193,13144582,13992914,14534641,13694869,15021846,16164913,17650591,18796955)
 CE_REAL=c(169968,162788,167268,165417,202905,218426,194919,220426,229478,274512,361951,423598,515587,528779)
 TE_REAL=c(1398033,1375973,1389881,1399220,1436073,1515882,1586647,1658390,1836179,2260036,2316118,2400204,2413605,2448851)
