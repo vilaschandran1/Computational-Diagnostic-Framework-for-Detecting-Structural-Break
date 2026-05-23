4afbd0f1b1b95e28d5cf8f0b92a8caf56a6a861b
vilaschandran1 <vilaschandran1@gmail.com>
2026-05-23 16:31:33 +0530

    Fix comments and print statement for total expenditure

--- 01_Main_Inidan_Macro_Fiscal_Analysis.R
+++ 01_Main_Inidan_Macro_Fiscal_Analysis.R
@@ -1,1 +1,56 @@
-
+RStudio Version-2025.09.2+418 "Cucumberleaf Sunflower" Release (12f6d5e22720bd78dbd926bb344efe12d0dce83d, 2025-10-20) for windows
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) RStudio/2025.09.2+418 Chrome/138.0.7204.251 Electron/37.6.1 Safari/537.36, Quarto 1.7.32
+set.seed(139)
+install.packages("urca")   #package for zivot-andrews unit root test
+install.packages("meboot") #package for maximum entropy bootstrap 
+install.packages("philentropy") #package for Jensen-Shannon Divergence Calculation
+library(meboot) 
+library(urca)
+library(philentropy)
+GDP_REAL=c(8736329,9213017,9801370,10527674,11369493,12308193,13144582,13992914,14534641,13694869,15021846,16164913,17650591,18796955)
+CE_REAL=c(169968,162788,167268,165417,202905,218426,194919,220426,229478,274512,361951,423598,515587,528779)
+TE_REAL=c(1398033,1375973,1389881,1399220,1436073,1515882,1586647,1658390,1836179,2260036,2316118,2400204,2413605,2448851)
+RE_REAL=c(1228065,1213184,1222613,1233803,1233168,1297455,1391728,1437965,1606701,1985524,1954167,1976607,1898018,1920072)
+##GDP_REAL=Real Gross Domestic Product,CE_REAL=Real Capital Expenditure,TE_REAL= Real Total Expenditure
+#RE_REAL=Real Revenue Expenditure
+ALL_INDICES=1:14
+######################APPLYNG MAXIMUM ENTROPY BOOTSTRAP ON REAL GDP TO GENERATE 999 PLAUSIBLE SERIES#################################
+repl_GDP=meboot(GDP_REAL,reps = 999)   #Maximum Entropy Bootstrap gdp
+repl_GDPENSE=repl_GDP$ensemble
+#######################EMBEDDING ZIVOT-ANDREWS UNIT ROOT TEST ON GENERATED 999 PLAUSIBLE SERIES FOR REAL GDP#########################
+repl_za_gdp=apply(repl_GDPENSE,2,function(x)
+  ur.za(x,model = "both",lag=1)@bpoint)                      #Zivot-Andrews Unit Root Test Model C
+repl_breakGDP= table(factor(repl_za_gdp,levels = ALL_INDICES))
+######################APPLYNG MAXIMUM ENTROPY BOOTSTRAP ON REAL REVENUE EXPENDITURE  TO GENERATE 999 PLAUSIBLE SERIES#################
+repl_RE=meboot(RE_REAL,reps = 999)       #Maximum Entropy Bootstrap revenue expenditure
+repl_REENSE=repl_RE$ensemble
+######################EMBEDDING ZIVOT-ANDREWS UNIT ROOT TEST ON GENERATED 999 PLAUSIBLE SERIES FOR REAL REVENUE EXPENDITURE###########
+repl_za_re=apply(repl_REENSE,2,function(x)
+  ur.za(x,model = "both",lag = 1)@bpoint)   #Zivot-Andrews Unit Root Test Model C
+repl_breakRE=table(factor(repl_za_re,levels = ALL_INDICES))
+#################APPLYNG MAXIMUM ENTROPY BOOTSTRAP ON REAL CAPITAL EXPENDITURE  TO GENERATE 999 PLAUSIBLE SERIES######################
+repl_CE=meboot(CE_REAL,reps = 999)      #Maximum Entropy Bootstrap capital expenditure
+repl_CEENSE=repl_CE$ensemble  
+######################EMBEDDING ZIVOT-ANDREWS UNIT ROOT TEST ON GENERATED 999 PLAUSIBLE SERIES FOR REAL CAPITAL EXPENDITURE###########
+repl_za_ce=apply(repl_CEENSE,2,function(x)
+  ur.za(x,model = "both",lag = 1)@bpoint)  #Zivot-Andrews Unit Root Test Model C
+repl_breakCE= table(factor(repl_za_ce,levels = ALL_INDICES))  
+#################APPLYNG MAXIMUM ENTROPY BOOTSTRAP ON REAL TOTAL EXPENDITURE  TO GENERATE 999 PLAUSIBLE SERIES#######################
+repl_TE=meboot(TE_REAL,reps = 999)     #Maximum Entropy Bootstrap Total Expenditure
+repl_TEENSE=repl_TE$ensemble
+######################EMBEDDING ZIVOT-ANDREWS UNIT ROOT TEST ON GENERATED 999 PLAUSIBLE SERIES FOR REAL TOTAL EXPENDITURE###########
+repl_za_te=apply(repl_TEENSE,2,function(x)
+  ur.za(x,model = "both",lag = 1)@bpoint)   #Zivot-Andrews Unit Root Test
+repl_breakTE=table(factor(repl_za_te,levels = ALL_INDICES)) 
+####################RESULTS BREAK-DATE FREQUENCY DISTRIBUTION#######################################################################
+print("---------------------------------Break-Date Frequency Distribution---------------------------------")
+#####################REAL GDP(BREAK-DATE)###########################################################################################                 
+print(repl_breakGDP) #Figure 1(refer main paper)
+#####################REAL REVENUE EXPENDITURE(Break-DATE)###########################################################################                 
+print(repl_breakRE) #Figure 1(refer main paper)
+#####################REAL CAPITAL EXPENDITURE(BREAK-DATE)###########################################################################                 
+print(repl_breakCE) #Figure 1(refer main paper)
+#####################REAL TOTAL EXPENDITURE(BREAK-DATE)#############################################################################                 
+print(repl_breakTE) #Figure 1(refer main paper)                
+                 
+                 
