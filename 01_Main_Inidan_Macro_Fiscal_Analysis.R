fea0f56c26ad8315406ced06553b3f1803718644
vilaschandran1 <vilaschandran1@gmail.com>
2026-06-11 18:59:17 +0530

    Clean up initial comments in R script
    Removed RStudio version and system information from the script.

--- 01_Main_Inidan_Macro_Fiscal_Analysis.R
+++ 01_Main_Inidan_Macro_Fiscal_Analysis.R
@@ -1,5 +1,3 @@
-RStudio Version-2025.09.2+418 "Cucumberleaf Sunflower" Release (12f6d5e22720bd78dbd926bb344efe12d0dce83d, 2025-10-20) for windows
-Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) RStudio/2025.09.2+418 Chrome/138.0.7204.251 Electron/37.6.1 Safari/537.36, Quarto 1.7.32
 set.seed(139)
 if(!require("urca")) install.packages("urca")
 if(!require("meboot")) install.packages("meboot")
